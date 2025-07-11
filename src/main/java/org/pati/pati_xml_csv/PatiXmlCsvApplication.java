17480108b146dacfbccbe67664942d7c11b5db2a
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-11 21:28:51 +0200

    initial commit

---
+++ src/main/java/org/pati/pati_xml_csv/PatiXmlCsvApplication.java
@@ -0,0 +1,13 @@
+package org.pati.pati_xml_csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatiXmlCsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatiXmlCsvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pati/pati_xml_csv/PatiXmlCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pati.pati_xml_csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatiXmlCsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
