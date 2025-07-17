60fb8e7be01c2a35837e53dfecd88616bdea8ee3
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 17:28:15 +0200

    Implement XmlToCsvRunner to trigger XML to CSV conversion on app start

--- src/main/java/org/pati/pati_xml_csv/component/XmlToCsvRunner.java
+++ src/main/java/org/pati/pati_xml_csv/component/XmlToCsvRunner.java
@@ -1,17 +1,33 @@
 package org.pati.pati_xml_csv.component;
 
 import lombok.AllArgsConstructor;
-import org.pati.pati_xml_csv.servie.ConversionService;
+import lombok.extern.slf4j.Slf4j;
+import org.pati.pati_xml_csv.service.ConversionService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @AllArgsConstructor
 public class XmlToCsvRunner implements CommandLineRunner {
 
     private final ConversionService conversionService;
+
     @Override
     public void run(String... args) throws Exception {
 
+        String xmlFilePathBook = "src/main/resources/xmlFilesToConvertToCsv/bookstore.xml";
+        String csvFilePathBook = "src/main/resources/convertedCsv/bookstore.csv";
+        conversionService.convertXmlToCsv(xmlFilePathBook, csvFilePathBook);
+
+        String xmlFilePathItems = "src/main/resources/xmlFilesToConvertToCsv/items.xml";
+        String csvFilePathItems = "src/main/resources/convertedCsv/items.csv";
+        conversionService.convertXmlToCsv(xmlFilePathItems, csvFilePathItems);
+
+        String xmlFilePathDevices = "src/main/resources/xmlFilesToConvertToCsv/devices.xml";
+        String csvFilePathDevices = "src/main/resources/convertedCsv/devices.csv";
+        conversionService.convertXmlToCsv(xmlFilePathDevices, csvFilePathDevices);
+
+        log.info("Conversion successful!");
     }
 }
