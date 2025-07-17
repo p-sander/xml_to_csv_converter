918739d699c929bd6467f7caf1d3cebba14fa40d
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 17:04:05 +0200

    Open XML file and initialize XMLStreamReader in try-with-resources, custom exception ParseXmlException

--- src/main/java/org/pati/pati_xml_csv/service/helper/XmlFileParser.java
+++ src/main/java/org/pati/pati_xml_csv/service/helper/XmlFileParser.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.pati.pati_xml_csv.exceptions.ParseXmlException;
 import org.pati.pati_xml_csv.model.ElementInfo;
 
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -22,6 +26,14 @@ public class XmlFileParser {
     public Map<String, Map<String, String>> parseXmlFile(String xmlFilePath, Set<String> headers) throws ParseXmlException {
         Map<String, Map<String, String>> recordsById = new LinkedHashMap<>();
         Stack<ElementInfo> elementStack = new Stack<>();
+
+        try (FileInputStream in = new FileInputStream(xmlFilePath)) {
+            XMLStreamReader reader = xmlParserHelper.initXmlReader(in);
+        } catch (IOException | XMLStreamException e) {
+            log.error("ParseXmlException occurred for file: {}", xmlFilePath, e);
+            throw new ParseXmlException("XML parsing error in " + xmlFilePath, e);
+        }
+
         return recordsById;
     }
 
