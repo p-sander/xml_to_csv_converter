eb73e2c6ee2725e37453e2f3958df60f0e1cc19a
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 17:23:55 +0200

    Implement sorting headers method

--- src/main/java/org/pati/pati_xml_csv/service/helper/CsvFileWriter.java
+++ src/main/java/org/pati/pati_xml_csv/service/helper/CsvFileWriter.java
@@ -13,44 +13,46 @@ import java.util.*;
 @Slf4j
 public class CsvFileWriter {
     public void writeDataToCsv(String csvFilePath, Set<String> headers, Map<String, Map<String, String>> records) throws WriteToCsvException {
+
+        List<String> sortedHeaders = prioritizeIdHeader(headers);
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath))) {
-            writer.write(String.join(",", headers));
+            writer.write(String.join(",", sortedHeaders));
             writer.newLine();
 
             for (Map<String, String> record : records.values()) {
                 List<String> rowValues = new ArrayList<>();
-                for (String header : headers) {
+                for (String header : sortedHeaders) {
                     String value = record.getOrDefault(header, "");
                     rowValues.add("\"" + value.replace("\"", "\"\"") + "\"");
                 }
                 writer.write(String.join(",", rowValues));
                 writer.newLine();
             }
-        } catch (IOException e) {
+        } catch (IOException e){
             log.error("WriteToCsvException occurred " + csvFilePath);
             throw new WriteToCsvException("An error occurred during writing data to csv file " + csvFilePath, e);
         }
     }
 
     private List<String> prioritizeIdHeader(Set<String> headers) {
-        validateIdExistenceInSet(headers);
+
+      validateIdExistenceInSet(headers);
 
         List<String> sortedHeaders = new LinkedList<>();
-        if (headers.contains("id")) {
-            sortedHeaders.add("id");
-        }
+
+        if (headers.contains("id")) sortedHeaders.add("id");
         for (String header : headers) {
-            if (!header.equals("id")) {
+            if (!sortedHeaders.contains(header)) {
                 sortedHeaders.add(header);
             }
         }
         return sortedHeaders;
     }
 
-    private void validateIdExistenceInSet(Set<String> headers) {
+    private void validateIdExistenceInSet(Set<String> headers){
         if (!headers.contains("id")) {
             throw new IllegalStateException("Missing 'id' header in XML data.");
         }
     }
-
 }
