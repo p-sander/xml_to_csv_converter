577a769d66f27490e2f4b2faed656c22427cb24b
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 15:41:27 +0200

    xml file parser set up

---
+++ src/main/java/org/pati/pati_xml_csv/service/helper/XmlFileParser.java
@@ -0,0 +1,14 @@
+package org.pati.pati_xml_csv.service.helper;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class XmlFileParser {
+
+    public Map<String, Map<String, String>> parseXmlFile(){
+        return null;
+    }
+}
+
