aedd7f3499f1f6ee30564228dcddc6f6ad88d0da
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 16:48:53 +0200

    Implement handleStartElement method for handling beginning of XML element (open tag)

--- src/main/java/org/pati/pati_xml_csv/service/helper/XmlElementHelper.java
+++ src/main/java/org/pati/pati_xml_csv/service/helper/XmlElementHelper.java
@@ -3,10 +3,30 @@ package org.pati.pati_xml_csv.service.helper;
 import lombok.NoArgsConstructor;
 import org.pati.pati_xml_csv.model.ElementInfo;
 
+import javax.xml.stream.XMLStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 @NoArgsConstructor
 public class XmlElementHelper {
+
+    private String handleStartElement(XMLStreamReader reader, Stack<ElementInfo> stack, String currentColumnKey) {
+        Map<String, String> attributes = new HashMap<>();
+
+        for (int i = 0; i < reader.getAttributeCount(); i++) {
+            attributes.put(reader.getAttributeLocalName(i), reader.getAttributeValue(i));
+        }
+
+        stack.push(new ElementInfo(reader.getLocalName(), attributes));
+
+        //get xxx data from tag attribute name="xxx"
+        if (attributes.containsKey("name")) {
+            return attributes.get("name");
+        }
+
+        return currentColumnKey;
+    }
 
     // use stack for storing all elements
     private String findRecordId(Stack<ElementInfo> stack) {
