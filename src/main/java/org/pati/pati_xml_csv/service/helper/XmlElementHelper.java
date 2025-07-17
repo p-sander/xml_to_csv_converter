8d17ea77728427c4899df9732677a360cbb69365
Patrycja Sander <patrycja.sander2@gmail.com>
2025-07-17 16:09:37 +0200

    XmlElementHelper set up

---
+++ src/main/java/org/pati/pati_xml_csv/service/helper/XmlElementHelper.java
@@ -0,0 +1,10 @@
+package org.pati.pati_xml_csv.service.helper;
+
+import lombok.NoArgsConstructor;
+import org.pati.pati_xml_csv.model.ElementInfo;
+
+import java.util.Stack;
+
+@NoArgsConstructor
+public class XmlElementHelper {
+}
